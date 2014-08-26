517126f65d660af15a63d3fce240311f176975bc
montoya <montoya@cern.ch>
2014-08-26 16:00:28 +0200

    fixing compilation bug for plotting

--- plot/XYquad.sh
+++ plot/XYquad.sh
@@ -7,6 +7,6 @@ export TMIN=`head -n 2 log.txt | tail -n 1 | awk '{print $9}'`
 export TMAX=`tail -n 1 log.txt | awk '{print $9}'`
 echo $value $TMIN  $TMAX
 cat XYquad.template | sed -e "s|-N-|$value|g" -e "s|-TMIN-|$TMIN|g" -e "s|-TMAX-|$TMAX|g" >  XYquad.C
-root -b XYquad.C
-export file=`ls -1 -tr XYquad*.png | tail -n 1`
-display $file
+root -l XYquad.C
+#export file=`ls -1 -tr XYquad*.png | tail -n 1`
+#display $file
